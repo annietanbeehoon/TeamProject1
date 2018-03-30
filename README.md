77e5554723c3a0893df5a2897e21d0e16485e92c
Annie Schaefer <annie@Annies-MacBook-Pro.local>
2018-03-30 10:44:38 -0400

    Second commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # TeamProject1
-teamProjectOne
+teamProjectOne - Week 4
+
+# Movie and Dinner Near Me
+
+## Summary
+In this project, we aim to achieve user friendly app for movie search, followed by the theatre near the user using geolocation. As an add-on, we will enhance the app to include a dinner search.
+
+## Backend - API used:
+1. The MovieDB API
+2. International Showtime API
+3. Google Maps API
+4. An API for restaurant search 
+
+## Backend - Coding Programs:
+1. CSS
+2. Bootstap
+3. Pure
+4. Javascript
+5. JQuery
+6. HTML
+
+## Frontend - User's APP view
+###View 1
+As an user, he/she will see an attractive front page to assist him/her with a movie submission form. The search terms are "Movie Title", "Actor", "Genre" and "Year". The user input the search term to generate result.
+
+###View 2
+Upon submission, it calls out a page showing the movie release date, plot/overview, poster and trailer. User will see the details in a neat and structured layout showing the details to help him with the decision - "Should I watch this movie?".
+
+###View 3
+User decides to watch the movie by submitting a button to call for movie theater near him. A modular box will appear for the user to choose to allow current location capture or self input zipcode to select the movie theatre near him/her.
+
+###View 4
+This page shows the theater currently showing the movie near him/her. It will show a google embed map view.
+
+A button will allow user to call up restuarants near him/her. THhs brings a nice closure to the user. A glass of wine to celebrate for a successful planning.
+
+## Task List
+
+[ ] Eric designs View 1 for the movie search
+[ ] Annie do AJAX for the TMDB for movie details
+[ ] Arthur designs View 2 & 3 for the container displaying movie details and movie theatre
+[ ] John do AJAX for the ShowTime for movie theatre by geolocation
+
+## Upcoming Task List - unassigned
+[ ] Research and code to create the modular user box
+[ ] A restaurant API search based on geolocation
+[ ] Embed the Google Maps' in the HTML
+
+
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,87 +1,102 @@
-// CLICK HANDLERS
-// ==========================================================
+$(document).ready(function () {
+  // CLICK HANDLERS
+  // ==========================================================
+  $("#movies-input").empty();
+  // .on("click") function associated with the Search Button
+  $(".pure-controls").on("click", function (event) {
+    // This line allows us to take advantage of the HTML "submit" property
+    // This way we can hit enter on the keyboard and it registers the search
+    // (in addition to clicks). Prevents the page from reloading on form submit.
+    event.preventDefault();
 
-// .on("click") function associated with the Search Button
-$(".pure-controls").on("click", function (event) {
-  // This line allows us to take advantage of the HTML "submit" property
-  // This way we can hit enter on the keyboard and it registers the search
-  // (in addition to clicks). Prevents the page from reloading on form submit.
-  event.preventDefault();
+    // queryURL is the url we'll use to query the API
+    var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=88887032dc1d0d80dd7ccbd783133865&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_cast&page=1append_to_response=overview,release_date,&query=";
+    //https://api.themoviedb.org/3/movie/157336?api_key={api_key}&append_to_response=videos,images
+    // grab text the user typed into the search input, add as parameter to url
+    //
+    //var movieInput = $("#input[type=text]").val();
+    //console.log(movieInput);
+    //movieInput = movieInput.replace(/ /g, '+');
+    // var movie = $("#movie-input").val().trim();
+    // var actor = $("#actor").val().trim();
+    // var genre = $("#genre").val().trim();
+    // var year = $("#year").val().trim();
+    // var movie = movie.replace(/ /g, '+');
+    // var actor = actor.replace(/ /g, '+');
+    // var genre = genre.replace(/ /g, '+');
+    // var year = year.replace(/ /g, '+');
 
-  // queryURL is the url we'll use to query the API
-  var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=88887032dc1d0d80dd7ccbd783133865&append_to_response=overview,release_date,&query=";
-  //https://api.themoviedb.org/3/movie/157336?api_key={api_key}&append_to_response=videos,images
-  // grab text the user typed into the search input, add as parameter to url
-  //
-  //var movieInput = $("#input[type=text]").val();
-  //console.log(movieInput);
-  //movieInput = movieInput.replace(/ /g, '+');
-  // var movie = $("#movie-input").val().trim();
-  // var actor = $("#actor").val().trim();
-  // var genre = $("#genre").val().trim();
-  // var year = $("#year").val().trim();
-  // var movie = movie.replace(/ /g, '+');
-  // var actor = actor.replace(/ /g, '+');
-  // var genre = genre.replace(/ /g, '+');
-  // var year = year.replace(/ /g, '+');
+    var movie = $("#movie-input").val().trim();
+    var movie = movie.replace(/ /g, '+');
+    console.log(movie);
 
-  var movie = $("#movie-input").val().trim();
-  console.log(movie);
+    console.log(movie);
+    queryURL += movie;
 
-  console.log(movie);
-  queryURL += movie;
-
-  // Logging the URL so we have access to it for troubleshooting
-  console.log("---------------\nURL: " + queryURL + "\n---------------");
-
-
-
-
-
-  // make the AJAX request to the API - GETs the JSON data at the queryURL.
-  // the data then gets passed as an argument to the function
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-
-    console.log(response);
-    //$("#movies-view").text(JSON.stringify(response));
-
-    //var movie = $(this).attr("data-name");
-
-    // Creating a div to hold the movie
-    var movieDiv = $("<div class='movie'>");
-
-    // Storing the imagesrelease date data
-    var releaseDate = response.results[0].release_date;
+    // Logging the URL so we have access to it for troubleshooting
+    console.log("---------------\nURL: " + queryURL + "\n---------------");
 
 
-    // Creating an element to have the release date displayed
-    var pOne = $("<p>").text("Release date: " + releaseDate);
+    // make the AJAX request to the API - GETs the JSON data at the queryURL.
+    // the data then gets passed as an argument to the function
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
 
-    // Displaying the releaseDate
-    movieDiv.append(pOne);
 
-    // Storing the overview
-    var overview = response.results[0].overview;
+      var movieId = JSON.parse(response.results[0].id);
 
-    // Creating an element to hold the overview
-    var pTwo = $("<p>").text("Overview: " + overview);
+      // Creating a div to hold the movie
+      var movieDiv = $("#movies-view");
 
-    // Appending the plot
-    movieDiv.append(pTwo);
+      // Storing the imagesrelease date data
+      var releaseDate = response.results[0].release_date;
 
-    // Retrieving the URL for the image
-    var imgURL = response.results[0].poster_path;
+      // Creating an element to have the release date displayed
+      var pOne = $("<p>").text("Release date: " + releaseDate);
 
-    // Creating an element to hold the image
-    var image = $("<img>").attr("src", imgURL);
+      // Displaying the releaseDate
+      movieDiv.append(pOne);
 
-    // Appending the image
-    movieDiv.append(image);
+      // Storing the overview
+      var overview = response.results[0].overview;
 
-    // Putting the entire movie above the previous movies
-    $("#movies-view").prepend(movieDiv);
-  });
-})
+      // Creating an element to hold the overview
+      var pTwo = $("<p>").text("Overview: " + overview);
+
+      // Appending the plot
+      movieDiv.append(pTwo);
+      // Retrieving the URL for the image
+
+      // $('#movie').focus(function() {
+      //     var full = 
+      //     $("#poster").has("img").length ? true :
+      //     false;
+      //     if(full === false) {
+      //       $("#poster").empty();
+      //     }
+      // });
+
+      //$.getJSON("https://image.tmdb.org/t/p/")
+
+      //var src1 = '<%=Url.Content(Server.MapPath("~/AppData/1.jpg"))%>';
+      //$("#imgLocation").attr("src", src1);
+
+      // https://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
+
+      // var imgURL = response.results[0].poster_path;
+      console.log('poster path: ', response.results[0].poster_path);
+      var imgURL = "https://image.tmdb.org/t/p/w500" + response.results[0].poster_path;
+      console.log(imgURL);
+      // Creating an element to hold the image
+      var image = $("<img>").attr("src", imgURL);
+
+      // Appending the image
+      movieDiv.append(image);
+
+      // Putting the entire movie above the previous movies
+      ////////////  $("#movies-view").prepend(movieDiv);
+    });
+  })
+});
