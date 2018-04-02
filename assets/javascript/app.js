4df89490d1db46e2b624e59c4e4c761598e03754
JohnGit54 <johnp701@yahoo.com>
2018-04-02 11:37:57 -0400

    updated add.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,19 +3,18 @@
 
 //global variables
 
-var ApiKey_iShowtimes = 'ax9iovcR67XrX8hGK1uRQpgGNymgCUaK';
-
+const ApiKey_iShowtimes = 'ax9iovcR67XrX8hGK1uRQpgGNymgCUaK';
+const ApiKey_googleMaps = "AIzaSyDSMDeXXQxaeLJ4ZGXuwSKAM3NHBP4ckTc";
 
 // test longitude and lat -- using my house
-var Latitude = 40.5953;
-var Longitude = -74.6173;
+var Latitude ; //= 40.5953;
+var Longitude ; //= -74.6173;
 var myTomorrow;
 
-var latLon = Latitude + "," + Longitude;
+// var latLon = Latitude + "," + Longitude;
 
 var arrCinema = [];
 var arrShowtimes = [];
-
 
 
 
@@ -23,7 +22,9 @@ var arrShowtimes = [];
 $(document).ready(function () {
     console.log("ready!");
 
-
+    // geoFindMe();
+    // map1();
+    // map2a();
     var movieID = $("#movieList").change(function () {
         console.log($(this).val());
         Ajax_withMovieID($(this).val());
